6dbe37f3e5a6021a6478adeac8d84005f15e16da
Ben Watkins <hi@bwatkins.dev>
2025-11-22 21:21:56 -0600

    switch data models to zod schemas

--- src/durable-objects/wheel-state/event/model.ts
+++ src/durable-objects/wheel-state/event/model.ts
@@ -1,13 +1,16 @@
-export type AddEntryWheelEvent = WheelEvent<'AddEntry', AddEntryEventData>;
-export interface AddEntryEventData {
-  label: string;
-}
+import { z } from 'zod';
 
+const AddEntryEventDataSchema = z.object({
+  label: z.string(),
+});
+export type AddEntryEventData = z.infer<typeof AddEntryEventDataSchema>;
+export type AddEntryWheelEvent = WheelEvent<'AddEntry', AddEntryEventData>;
+
+const RemoveEntryEventDataSchema = z.object({
+  label: z.string(),
+});
+export type RemoveEntryEventData = z.infer<typeof RemoveEntryEventDataSchema>;
 export type RemoveEntryWheelEvent = WheelEvent<'RemoveEntry', RemoveEntryEventData>;
-export interface RemoveEntryEventData {
-  label: string;
-  idk: boolean;
-}
 
 export type WheelEvent<Name extends string, Data extends object> = {
   name: Name;
